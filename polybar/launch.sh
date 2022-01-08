c24d95c30ac2b56e84fbaf2eb7586ab11d8c188f
Kimbix <alemanhumberto06@gmail.com>
2022-01-07 20:48:19 -0400

    did a lot of things, mainly rofi

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -20,15 +20,3 @@ polybar top_second 2>&1 | tee -a /tmp/polybar2.log & disown
 ## Launch the keyboard layout changer
 setxkbmap -layout us,es
 setxkbmap -option 'grp:alt_space_toggle'
-
-## Syncs the configuration files with the git repo
-CONF='/home/humberto/.config'
-REPO='/home/humberto/Documents/Repos/My-Linux-Stuf'
-rsync $CONF/polybar/config $REPO/polybar/config
-rsync $CONF/polybar/launch.sh $REPO/polybar/launch.sh
-
-rsync $CONF/i3/config $REPO/i3/config
-
-rsync $CONF/rofi/config.rasi $REPO/rofi/config.rafi
-rsync $CONF/rofi/configlauncher $REPO/rofi/configlauncher
-rsync $CONF/rofi/powermenu $REPO/rofi/powermenu
